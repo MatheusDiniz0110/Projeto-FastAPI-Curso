26b23863e593113eb2c202d428a624f386496d18
Matheus Diniz Tereza <matheus.diniz.t@outlook.com>
2026-04-06 11:34:06 -0300

    Tentando arrumar os erros

--- order_routes.py
+++ order_routes.py
@@ -15,8 +15,8 @@ async def orders():
 
 @order_router.post("/pedidos")
 async def criar_pedido(pedido_schema: PedidoSchema, session: Session = Depends(pegar_sessao)):
-    novo_pedido = Pedidos(usuario=pedido_schema.id_usuario)
+    novo_pedido = Pedidos(usuario=pedido_schema.usuario)
     session.add(novo_pedido)
     session.commit()
-    return {"message": f"Pedido criado com sucesso para o usuário {novo_pedido.id_usuario}"}
+    return {"message": f"Pedido criado com sucesso para o usuário {novo_pedido.usuario}"}
     
