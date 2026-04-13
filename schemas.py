29cc93cd00e2ea696114b38c36f11bca86d278e4
Matheus Diniz Tereza <matheus.diniz.t@outlook.com>
2026-04-13 08:29:56 -0300

    OAuth2 sendo utilizado para poder fazer login do usuario

---
+++ schemas.py
@@ -0,0 +1,25 @@
+from pydantic import BaseModel
+from typing import Optional
+
+class UserSchema(BaseModel):
+    name: str
+    email: str
+    password: str
+    active: Optional[bool] = True
+    admin: Optional[bool] = False
+
+    class Config:
+        from_attributes = True
+
+class OrderSchema(BaseModel):
+    user_id: int
+
+    class Config:
+        from_attributes = True
+
+class LoginSchema(BaseModel):
+    email: str
+    password: str
+
+    class Config:
+        from_attributes = True
